4de367ae6dc3cdeccc17fcd52f268218fbd9615c
jdassd <jdassd@163.com>
2020-04-16 20:44:50 +0800

    2020/4/16 详情页面著作权显示

--- src/main/java/com/James/corporateportraitplatforms/controller/ShowDataController.java
+++ src/main/java/com/James/corporateportraitplatforms/controller/ShowDataController.java
@@ -2,9 +2,11 @@ package com.James.corporateportraitplatforms.controller;
 
 import com.James.corporateportraitplatforms.mapper.CompanyMapper;
 import com.James.corporateportraitplatforms.mapper.CompanyShowDataMapper;
+import com.James.corporateportraitplatforms.mapper.KnowledgeReportMapper;
 import com.James.corporateportraitplatforms.model.AjaxResponseModel;
 import com.James.corporateportraitplatforms.model.Company;
 import com.James.corporateportraitplatforms.model.CompanyShowData;
+import com.James.corporateportraitplatforms.model.KnowledgeReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,9 @@ public class ShowDataController {
     @Autowired
     private CompanyMapper companyMapper;
 
+    @Autowired
+    private KnowledgeReportMapper knowledgeReportMapper;
+
     @GetMapping("/get-company-show-data-list")
     public AjaxResponseModel<List<CompanyShowData>> getCompanyShowDataList(@RequestParam String cid){
         return AjaxResponseModel.<List<CompanyShowData>>builder()
@@ -37,5 +42,13 @@ public class ShowDataController {
                 .code(0)
                 .data(companyMapper.selectByPrimaryKey(cid))
                 .msg("getting companyDataList successfully").build();
+    }
+
+    @GetMapping("/get-knowledge")
+    public AjaxResponseModel<KnowledgeReport> getKnowledge(@RequestParam String cid){
+        return AjaxResponseModel.<KnowledgeReport>builder()
+                .code(0)
+                .data(knowledgeReportMapper.selectByCid(cid))
+                .msg("getting knowledgeReport successfully").build();
     }
 }
